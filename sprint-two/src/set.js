8d564739d64d4a1f0e4bc8c727f935ede376f0de
Benjamin Lee <leebyp@gmail.com>
2014-04-17 16:55:23 -0700

    Done sets and hashtables(basic).

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,22 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+	if (this._storage[item]){
+		return true;
+	}
+	return false;
 };
 
 setPrototype.remove = function(item){
+	delete this._storage[item];
 };
