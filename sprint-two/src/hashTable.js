41d15623822cbd933410fa0203744648b9940a47
Benjamin Lee <leebyp@gmail.com>
2014-04-20 21:02:57 -0700

    Added hash tables theory.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,16 +6,6 @@ var HashTable = function(){
 //k key, v value
 
 HashTable.prototype.insert = function(k, v){
-  /*var amount = 0;
-  this._storage.each(function(value, key, storage){
-  	if (value){
-  		amount++;
-  	}
-  })
-  if (amount > this._limit){
-  	this._limit *= 2;
-  }*/
-
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(i, v);
 };
@@ -26,23 +16,80 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
-  /*var amount = 0;
-  this._storage.each(function(value, key, storage){
-  	if (value){
-  		amount++;
-  	}
-  })
-  if (amount <= this._limit/2){
-  	this._limit /= 2;
-  }
-
-  var removeArrayElement = function(array, index){
-  	array.splice(index, 1, null);
-  } 
-
-  this._storage.each(function(value, key, storage){
-  	removeArrayElement(storage, k);
-  })*/
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(i, null);
 };
+
+
+open hashing(separate chaining),
+key => index(bucketarray) => entries with same index form a linked list within same bucketarray
+time = constant time + list time
+//worse case scenario list time (n)
+//having an ordered list, approx 1/2 cost unsuccessful lookups
+//unordered list with move-to-front, if some keys more likely
+
+
+array hash table (using a bucket array),
+key => index(bucketarray) => entries with same index form a dynamic array within the bucketarray
+//dynamic array, grown only by need bytes
+
+
+closed hashing(open addressing),
+key => index(bucketarray) hashed-to slot => /*some probe sequence*/ until empty slot found
+//when search, scan bucket in same sequence starting at first hash location, until target found, or unused array slot found (no such key)
+//when found, use the original key to decode!
+//linear probing,fixed intervals (usually 1)
+//quadratic probing, interval increased by adding successive outputs of quadratic to starting value
+//double hashing, interval computed by another hash function within the particular bucket
+
+how to know which key at the particular index, and which were added on, using closed hashing?
+eg. hash function, string.length
+insert:
+	dog => 3, store ['dog', 'puppy'] in array[3]
+	cat => 3 => 4, store ['cat', 'kitten'] in array[4]
+	dog => 3, array[3][0] is 'dog', overwrite
+retrieve:
+	dog => 3, check array[3][0] === dog, success
+	cat => 3, check array[3][0] === cat, fail, check array[4,0], success
+USE TUPLES!!!
+
+
+. even with perfect hashing function,
+	.. no unlimited memory => therefore will need keys at some time
+		... can never avoid collisions
+
+
+. completely constant hashtable, hashing again at every bucket
+	.. therefore down to the hashing function
+	.. birthday problem => will have some collision
+		.. but unlikely more than 2/3 collisions
+		.. >75%, likely to have collisions mathematically, resize array
+			.. birthday problem
+
+. when resize array, have to rehash everything
+	.. since modulo will be wrong for new rehash, whether >75%(double) or <37.5%(half)
+	.. for dynamic arrays, not linked chains (which would screw up linkages)
+		... worth having array['dog', 'puppy', Math.floor(index/modulo)]??? to rehash quickly?
+		... cannot do that because halving the hash table would screw things up
+
+
+output of hashfunc 14
+%6 								at index 2, ['dog','puppy', Math.floor(14/6]]
+say if doubled size	to 12		at index (2*6)+2 => 14 => %12 => at index 2				
+
+
+this._storage  = limitedArray, []
+
+insert,
+retrieve,
+remove,
+
+
+
+
+
+
+
+
+
+
